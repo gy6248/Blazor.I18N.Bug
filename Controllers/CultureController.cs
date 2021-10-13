fdf30bd2fc0aea990703c84bf650ee52cf8a0c60
Audun Skjellnes <audun.skjellnes@sandvik.com>
2021-10-13 12:15:16 +0200

    Add project files.

---
+++ Controllers/CultureController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+
+namespace Blazor.I18N.Bug.Data
+{
+    [Route("[controller]/[action]")]
+    public class CultureController : Controller
+    {
+        public IActionResult SetCulture(string culture, string redirectUri)
+        {
+            if (culture != null)
+            {
+                var requestCulture = new RequestCulture(culture, culture);
+
+                HttpContext.Response.Cookies.Append(
+                    CookieRequestCultureProvider.DefaultCookieName,
+                    CookieRequestCultureProvider.MakeCookieValue(requestCulture));
+
+            }
+
+            return LocalRedirect(redirectUri);
+        }
+    }
+}
---
+++ Controllers/Resource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blazor.I18N.Bug
+{
+    public class Resource
+    {
+    }
+}
