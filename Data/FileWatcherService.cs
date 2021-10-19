0e81e854b2fc131743e5c9a62fdcfa84974711fb
Audun Skjellnes <audun.skjellnes@sandvik.com>
2021-10-19 15:43:30 +0200

    add a lot of debugging

--- Data/FileWatcherService.cs
+++ Data/FileWatcherService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.IO;
+using System.Globalization;
 
 namespace Blazor.I18N.Bug.Data
 {
@@ -13,11 +14,19 @@ namespace Blazor.I18N.Bug.Data
         public FileWatcherService()
         {
             FileWatcher = new FileSystemWatcher(WATCHED_PATH);
+            Thread = Environment.CurrentManagedThreadId;
+            Culture = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
         }
 
         public FileSystemWatcher FileWatcher { get; } 
 
         public FileSystemWatcher SecondWatcher { get; set; }
+
+        public int Thread { get; }
+
+        public string Culture { get; }
+
+        public string CurrentUICulture { get { return CultureInfo.CurrentUICulture.IetfLanguageTag; } }
 
     }
 }
